e6e0050b4437cd5b8ff51d72a1642a00d1e4734c
BIKASH KUMAR SHARMA <bikashsharma5151@gmail.com>
2024-09-10 23:50:41 +0530

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,19 @@
+// function sendemail(){
+//     Email.send({
+//         Host : "smtp.gmail.com",
+//         Username : "bikashsharma5551@gmail.com",
+//         Password : "bikashsharma215#",
+//         To : 'bikashsharma5151@gmail.com',
+//         From : document.getElementsByClassName("eml").value,
+//         Subject : "for hiring as designer or developer",
+//         Body : "Message:"+document.getElementsByClassName("message").value
+//     }).then(
+//       message => alert("Thanks, I will get in touch with you")
+//     );
+// }
+
+var w = window.innerWidth;
+const tx=document.getElementsByClassName("txt");
+if (w<=600){
+    document.getElementById('test').innerHTML = <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Fira+Code&weight=600&pause=1000&color=20232BB9&width=300&height=40&lines=I'm+Bikash+sharma+Designer+;and+Frontend+Developer" alt="Typing SVG" /></a>;
+}
